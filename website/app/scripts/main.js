2c46aad2a1a9b33a13ef4d2f2452e43d430fdc44
Ben Wiedermann <benw@cs.hmc.edu>
2015-05-24 17:13:06 -0700

    Working website!

--- website/app/scripts/main.js
+++ website/app/scripts/main.js
@@ -1,5 +1,25 @@
+'use strict';
+
 var lambdaTreesApp = angular.module('lambdaTreesApp', []);
 
 lambdaTreesApp.controller('VisController', ['$scope', function($scope) {
-     
+
+    // taken from: http://pegjs.org/js/online.js
+    function buildErrorMessage(e) {
+      return e.line !== undefined && e.column !== undefined
+        ? "Line " + e.line + ", column " + e.column + ": " + e.message
+        : e.message;
+    }
+
+     $scope.displayExpression = function() {
+      var result;
+      try {
+       result = parse($scope.expression);
+       $scope.errorMessage = '';
+       drawTree('#tree-vis', result);
+      } catch (e) {
+       $scope.errorMessage = buildErrorMessage(e);
+      }
+     };
+
    }]);
